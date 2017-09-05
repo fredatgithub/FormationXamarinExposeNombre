0aae88f12add610f86f5b906c2fb043ea0a20260
Fred <domaine75@gmail.com>
2017-09-05 12:52:52 +0100

    initial commit

---
+++ ExposeNombre/ExposeNombre/ExposeNombreXaml.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ExposeNombre
+{
+    public partial class ExposeNombreXaml : ContentPage
+    {
+        public ExposeNombreXaml()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtNum_OnFocused(object sender, FocusEventArgs e)
+        {
+            
+        }
+
+        private void TxtExp_OnFocused(object sender, FocusEventArgs e)
+        {
+            
+        }
+
+        private void BtnExpose_OnClicked(object sender, EventArgs e)
+        {
+            var num = double.Parse(txtNum.Text);
+            var exp = double.Parse(txtExp.Text);
+            var result = Math.Pow(exp, num);
+            var info = $"Résultat : {result}";
+            lblResult.IsVisible = true;
+            lblResult.Text = info;
+        }
+    }
+}
---
+++ ExposeNombre/ExposeNombre/ExposeNombre_code.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ExposeNombre
+{
+    public class ExposeNombreCode : ContentPage
+    {
+        public ExposeNombreCode()
+        {
+            Title = "ExposeNombre";
+            var txtNum = new Entry {Placeholder =  "nombre", Text = "2"};
+            var txtExp = new Entry {Placeholder = "exposant", Text = "3"};
+            var btnExpose = new Button {Text = "Expose C"};
+            var lblResult = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold
+            };
+
+            //btnExpose.Clicked += btnExpose_Clicked;
+            btnExpose.Clicked += (sender, e) =>
+            {
+                var num = double.Parse(txtNum.Text);
+                var exp = double.Parse(txtExp.Text);
+                var result = Math.Pow(exp, num);
+                var info = $"Résultat : {result}";
+                lblResult.Text = info;
+            };
+
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = {txtNum, txtExp, btnExpose, lblResult }
+                
+            };
+        }
+    }
+}
